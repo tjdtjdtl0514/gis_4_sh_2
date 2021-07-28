e1beb21d8862ea78ec5245f3bd9ad60d073d38c5
kim sung hoon <tjdtjdtl0514@likelion.org>
2021-07-29 08:46:54 +0900

    Initial commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 
@@ -16,16 +16,12 @@ from accountapp.models import NewModel
 @login_required
 def hello_world(request):
     if request.method == "POST":
-
         temp = request.POST.get('next')
-        request.GET.get('next')
 
         new_model = NewModel()
         new_model.text = temp
         new_model.save()
-
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
     else:
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
@@ -45,18 +41,23 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+has_ownership = [login_required, account_ownership_required]
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
--- gis_4ban_sh2/urls.py
+++ gis_4ban_sh2/urls.py
@@ -1,4 +1,4 @@
-"""gis_4ban_1 URL Configuration
+"""gis_4ban_sh2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls'))
-]
+    path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
